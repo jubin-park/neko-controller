ced865a721c8b0fb5aa501975992b6b28959c2b9
Jubin Park <jubin-park@naver.com>
2020-02-14 02:27:36 +0900

    Add load-bitmap feature in ControlDirection4

--- rpgxp/Data/script/control-direction4.rb
+++ rpgxp/Data/script/control-direction4.rb
@@ -1,3 +1,51 @@
 class ControlDirection4 < ControlInterface
-    
+
+  attr_reader(:sprite_stick)
+  attr_reader(:bitmap_default)
+  attr_reader(:bitmap_down)
+  attr_reader(:bitmap_left)
+  attr_reader(:bitmap_right)
+  attr_reader(:bitmap_up)
+  attr_reader(:bitmap_stick)
+  attr_reader(:stick_movable_radius)
+
+  def initialize(x, y, z, rect_touchable = true)
+    super([2, 4, 6, 8], x, y, z, rect_touchable)
+    @sprite_stick = Sprite.new(Controller.viewport)
+    @sprite_stick.z = @sprite.z + 1
+    @sprite_stick.visible = false
+  end
+
+  def set_image_default(bitmap_or_path)
+    @bitmap_default = ControlInterface.get_bitmap(bitmap_or_path)
+    @sprite.bitmap ||= @bitmap_default
+  end
+
+  def set_image_down(bitmap_or_path)
+    @bitmap_down = ControlInterface.get_bitmap(bitmap_or_path)
+  end
+
+  def set_image_left(bitmap_or_path)
+    @bitmap_left = ControlInterface.get_bitmap(bitmap_or_path)
+  end
+
+  def set_image_right(bitmap_or_path)
+    @bitmap_right = ControlInterface.get_bitmap(bitmap_or_path)
+  end
+
+  def set_image_up(bitmap_or_path)
+    @bitmap_up = ControlInterface.get_bitmap(bitmap_or_path)
+  end
+
+  def set_image_stick(radius, bitmap_or_path)
+    @stick_movable_radius = radius
+    @bitmap_stick = ControlInterface.get_bitmap(bitmap_or_path)
+    @sprite_stick.bitmap = @bitmap_stick
+    @sprite_stick.ox = @bitmap_stick.width / 2
+    @sprite_stick.oy = @bitmap_stick.height / 2
+    @sprite_stick.x = @sprite.x + @sprite_stick.ox
+    @sprite_stick.y = @sprite.y + @sprite_stick.oy
+    @sprite_stick.visible = true
+  end
+
 end
--- rpgxp/Data/script/main.rb
+++ rpgxp/Data/script/main.rb
@@ -5,18 +5,28 @@ require "kernel"
 require "sdl"
 #require "sdl-key"
 require "rpg-cache"
+require "controller"
 require "control-interface"
 require "control-button"
-require "controller"
+require "control-direction4"
+
 
 $button1 = ControlButton.new(SDL::Key::LEFT, 128, 128, 1)
 $button1.set_image_default("Graphics/NekoControls/attack_0.png")
 $button1.set_image_pressed("Graphics/NekoControls/attack_1.png")
 
-$button2 = ControlButton.new(SDL::Key::RIGHT, 200, 128, 1)
+$button2 = ControlButton.new(SDL::Key::RIGHT, 170, 128, 1)
 $button2.set_image_default("Graphics/NekoControls/attack_0.png")
 $button2.set_image_pressed("Graphics/NekoControls/attack_1.png")
 
-$button3 = ControlButton.new(SDL::Key::ESCAPE, 640 - 66, 480 - 66, 1, false)
+$button3 = ControlButton.new(SDL::Key::ESCAPE, Graphics.width - 66, Graphics.height - 66, 2, true)
 $button3.set_image_default(RPG::Cache.neko_control("UltimateDroidButton4.png"))
 $button3.set_image_pressed(RPG::Cache.neko_control("UltimateDroidButton4Pressed"))
+
+$dpad = ControlDirection4.new(0, Graphics.height - 224, 5)
+$dpad.set_image_default(RPG::Cache.neko_control("dpad_none"))
+$dpad.set_image_down(RPG::Cache.neko_control("dpad_down"))
+$dpad.set_image_left(RPG::Cache.neko_control("dpad_left"))
+$dpad.set_image_right(RPG::Cache.neko_control("dpad_right"))
+$dpad.set_image_up(RPG::Cache.neko_control("dpad_up"))
+$dpad.set_image_stick(16, RPG::Cache.neko_control("dpad_stick"))
