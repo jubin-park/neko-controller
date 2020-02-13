437af3b9d14522003198754214753a45c024316b
Jubin Park <jubin-park@naver.com>
2020-02-13 14:44:44 +0900

    Fix main.rb

--- rpgxp/Data/script/main.rb
+++ rpgxp/Data/script/main.rb
@@ -5,9 +5,10 @@ require "kernel"
 require "sdl"
 #require "sdl-key"
 require "rpg-cache"
+require "control-interface"
 require "control-button"
 require "controller"
 
-$button1 = ControlButton.new(SDL::Key::F2, 128, 128, 1, "Graphics/NekoControls/key5_0.png")
-$button2 = ControlButton.new(SDL::Key::RIGHT, 200, 128, 1, "Graphics/NekoControls/key5_1.png")
+$button1 = ControlButton.new(SDL::Key::F2, 128, 128, 1, "Graphics/NekoControls/attack_0.png")
+$button2 = ControlButton.new(SDL::Key::RIGHT, 200, 128, 1, "Graphics/NekoControls/attack_1.png")
 $button3 = ControlButton.new(SDL::Key::ESCAPE, 0, 0, 1, RPG::Cache.neko_control("dpad_menu.png"))
