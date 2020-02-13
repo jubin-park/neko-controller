88edad4f95ff9a4542559188527a723982f1ad1c
Jubin Park <jubin-park@naver.com>
2020-02-14 01:25:43 +0900

    Fix ControlButton class

--- rpgxp/Data/script/control-button.rb
+++ rpgxp/Data/script/control-button.rb
@@ -1,17 +1,46 @@
 class ControlButton < ControlInterface
 
-  def initialize(key, x, y, z, bitmap_or_path)
-    super(key, x, y, z)
-    if bitmap_or_path.is_a?(Bitmap)
-      @width = bitmap_or_path.width
-      @height = bitmap_or_path.height
-      @sprite.bitmap = bitmap_or_path
-    elsif bitmap_or_path.is_a?(String)
-      b = Bitmap.new(bitmap_or_path)
-      @width = b.width
-      @height = b.height
-      @sprite.bitmap = b
+  attr_reader(:bitmap_default)
+  attr_reader(:bitmap_pressed)
+
+  def initialize(key, x, y, z, rect_touchable = true)
+    super(key, x, y, z, rect_touchable)
+    @bitmap_default = nil
+    @bitmap_pressed = nil
+  end
+
+  def set_image_default(bitmap_or_path)
+    @bitmap_default = if bitmap_or_path.is_a?(Bitmap)
+        bitmap_or_path
+      elsif bitmap_or_path.is_a?(String)
+        Bitmap.new(bitmap_or_path)
+      end
+    @sprite.bitmap = @bitmap_default
+  end
+
+  def set_image_pressed(bitmap_or_path)
+    @bitmap_pressed = if bitmap_or_path.is_a?(Bitmap)
+        bitmap_or_path
+      elsif bitmap_or_path.is_a?(String)
+        Bitmap.new(bitmap_or_path)
+      end
+  end
+
+  def self.listen(finger_id, x, y, type, control)
+    # send key event
+    case type
+    when TouchType::DOWN
+      Controller.send_event(SDL::Event::KeyDown, control.key, true)
+      control.first_pressed = true
+      control.sprite.bitmap = control.bitmap_pressed
+
+    when TouchType::UP
+      Controller.send_event(SDL::Event::KeyUp, control.key, false)
+      control.first_pressed = false
+      control.sprite.bitmap = control.bitmap_default
+
+    when TouchType::DRAG
     end
   end
-  
+
 end
