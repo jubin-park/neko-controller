dafce714c2a251183e83fd57a3f922cb365e46b5
Jubin Park <jubin-park@naver.com>
2020-02-16 17:24:41 +0900

    Add `ControlDirection8` class

---
+++ rpgxp/Data/script/control-direction8.rb
@@ -0,0 +1,150 @@
+class ControlDirection8 < ControlInterface
+
+  attr_reader(:sprite_stick)
+  attr_reader(:bitmap_default)
+  attr_reader(:bitmap_lower_left)
+  attr_reader(:bitmap_down)
+  attr_reader(:bitmap_lower_right)
+  attr_reader(:bitmap_left)
+  attr_reader(:bitmap_right)
+  attr_reader(:bitmap_upper_left)
+  attr_reader(:bitmap_up)
+  attr_reader(:bitmap_upper_right)
+  attr_reader(:bitmap_stick)
+  attr_reader(:stick_movable_radius)
+
+  def initialize(x, y, z, rect_touchable = true)
+    super([], x, y, z, rect_touchable)
+    @sprite_stick = Sprite.new(Controller.viewport)
+    @sprite_stick.z = @sprite.z + 1
+    @sprite_stick.visible = false
+    @stick_center_x = 0
+    @stick_center_y = 0
+  end
+
+  def set_image_default(bitmap_or_path)
+    @bitmap_default = ControlInterface.get_bitmap(bitmap_or_path)
+    @sprite.bitmap ||= @bitmap_default
+  end
+
+  def set_image_lower_left(bitmap_or_path)
+    @bitmap_lower_left = ControlInterface.get_bitmap(bitmap_or_path)
+  end
+
+  def set_image_down(bitmap_or_path)
+    @bitmap_down = ControlInterface.get_bitmap(bitmap_or_path)
+  end
+
+  def set_image_lower_right(bitmap_or_path)
+    @bitmap_lower_right = ControlInterface.get_bitmap(bitmap_or_path)
+  end
+
+  def set_image_left(bitmap_or_path)
+    @bitmap_left = ControlInterface.get_bitmap(bitmap_or_path)
+  end
+
+  def set_image_right(bitmap_or_path)
+    @bitmap_right = ControlInterface.get_bitmap(bitmap_or_path)
+  end
+
+  def set_image_upper_left(bitmap_or_path)
+    @bitmap_upper_left = ControlInterface.get_bitmap(bitmap_or_path)
+  end
+
+  def set_image_up(bitmap_or_path)
+    @bitmap_up = ControlInterface.get_bitmap(bitmap_or_path)
+  end
+
+  def set_image_upper_right(bitmap_or_path)
+    @bitmap_upper_right = ControlInterface.get_bitmap(bitmap_or_path)
+  end
+
+  def set_image_stick(radius, bitmap_or_path)
+    @stick_movable_radius = radius
+    @bitmap_stick = ControlInterface.get_bitmap(bitmap_or_path)
+    @sprite_stick.bitmap = @bitmap_stick
+    @sprite_stick.ox = @bitmap_stick.width / 2
+    @sprite_stick.oy = @bitmap_stick.height / 2
+    @stick_center_x = @sprite.x + (@sprite.bitmap.width - @bitmap_stick.width) / 2 + @sprite_stick.ox
+    @stick_center_y = @sprite.y + (@sprite.bitmap.height - @bitmap_stick.height) / 2 + @sprite_stick.oy
+    @sprite_stick.x = @stick_center_x
+    @sprite_stick.y = @stick_center_y
+    @sprite_stick.visible = true
+  end
+
+  def move_stick(touch_x, touch_y)
+    dx = @stick_center_x - touch_x
+    dy = @stick_center_y - touch_y
+    dist2 = dx * dx + dy * dy
+    r = @stick_movable_radius
+    if dist2 < r * r
+      @sprite_stick.x = touch_x
+      @sprite_stick.y = touch_y
+    else
+      angle = Math.atan2(dy, dx) + Math::PI
+      @sprite_stick.x = @stick_center_x + r * Math.cos(angle)
+      @sprite_stick.y = @stick_center_y + r * Math.sin(angle)
+    end
+  end
+
+  def event_touch_over(finger_id, x, y, type)
+    super(finger_id, x, y, type)
+    @sprite.bitmap = @bitmap_default
+    @sprite_stick.x = @stick_center_x
+    @sprite_stick.y = @stick_center_y
+    @key.each { |k| Controller.send_event(SDL::Event::KeyUp, k, false) }
+    @key = []
+  end
+
+  def event_touch_in(finger_id, x, y, type)
+    case type
+    when Controller::TouchType::DOWN, Controller::TouchType::DRAG
+      @first_pressed = true
+      dx = @sprite.x + @sprite.bitmap.width / 2 - x
+      dy = @sprite.y + @sprite.bitmap.height / 2 - y
+      angle = Math.atan2(dy, dx) * (180 / Math::PI)
+      case angle
+      when (-67.5...-22.5)
+        @sprite.bitmap = @bitmap_lower_left
+        sdl_key = [SDL::Key::DOWN, SDL::Key::LEFT]
+      when (-112.5...-67.5)
+        @sprite.bitmap = @bitmap_down
+        sdl_key = [SDL::Key::DOWN]
+      when (-157.5...-112.5)
+        @sprite.bitmap = @bitmap_lower_right
+        sdl_key = [SDL::Key::DOWN, SDL::Key::RIGHT]
+      when (-22.5...22.5)
+        @sprite.bitmap = @bitmap_left
+        sdl_key = [SDL::Key::LEFT]
+      when (157.5...180.0), (-180.0...-157.5)
+        @sprite.bitmap = @bitmap_right
+        sdl_key = [SDL::Key::RIGHT]
+      when (22.5...67.5)
+        @sprite.bitmap = @bitmap_upper_left
+        sdl_key = [SDL::Key::UP, SDL::Key::LEFT]
+      when (67.5...112.5)
+        @sprite.bitmap = @bitmap_up
+        sdl_key = [SDL::Key::UP]
+      when (112.5...157.5)
+        @sprite.bitmap = @bitmap_upper_right
+        sdl_key = [SDL::Key::UP, SDL::Key::RIGHT]
+      end
+      if @key != sdl_key
+        @key.each { |k| Controller.send_event(SDL::Event::KeyUp, k, false) }
+        @key = sdl_key
+        @key.each { |k| Controller.send_event(SDL::Event::KeyDown, k, true) }
+      end
+      move_stick(x, y) if @sprite_stick.visible && @sprite_stick.opacity > 0
+
+    when Controller::TouchType::UP
+      @first_pressed = false
+      @sprite.bitmap = @bitmap_default
+      @sprite_stick.x = @stick_center_x
+      @sprite_stick.y = @stick_center_y
+      @key.each { |k| Controller.send_event(SDL::Event::KeyUp, k, false) }
+      @key = []
+
+    end
+  end
+
+end
--- rpgxp/Data/script/main.rb
+++ rpgxp/Data/script/main.rb
@@ -9,24 +9,51 @@ require "controller"
 require "control-interface"
 require "control-button"
 require "control-direction4"
+require "control-direction8"
 
-
-$button1 = ControlButton.new(SDL::Key::LEFT, 128, 128, 1)
+$button1 = ControlButton.new(SDL::Key::LEFT, 148, 348, 1)
 $button1.set_image_default("Graphics/NekoControls/attack_0.png")
 $button1.set_image_pressed("Graphics/NekoControls/attack_1.png")
 
-$button2 = ControlButton.new(SDL::Key::RIGHT, 170, 128, 1)
+$button2 = ControlButton.new(SDL::Key::RIGHT, 190, 348, 1)
 $button2.set_image_default("Graphics/NekoControls/attack_0.png")
 $button2.set_image_pressed("Graphics/NekoControls/attack_1.png")
 
-$button3 = ControlButton.new(SDL::Key::ESCAPE, Graphics.width - 66, Graphics.height - 66, 2, true)
-$button3.set_image_default(RPG::Cache.neko_control("UltimateDroidButton4.png"))
-$button3.set_image_pressed(RPG::Cache.neko_control("UltimateDroidButton4Pressed"))
+$button3 = ControlButton.new(SDL::Key::RETURN, 182, 400, 2)
+$button3.set_image_default(RPG::Cache.neko_control("default/UltimateDroidButton3"))
+$button3.set_image_pressed(RPG::Cache.neko_control("default/UltimateDroidButton3Pressed"))
 
-$dpad = ControlDirection4.new(0, Graphics.height - 224, 5)
-$dpad.set_image_default(RPG::Cache.neko_control("dpad_none"))
-$dpad.set_image_down(RPG::Cache.neko_control("dpad_down"))
-$dpad.set_image_left(RPG::Cache.neko_control("dpad_left"))
-$dpad.set_image_right(RPG::Cache.neko_control("dpad_right"))
-$dpad.set_image_up(RPG::Cache.neko_control("dpad_up"))
-$dpad.set_image_stick(24, RPG::Cache.neko_control("dpad_stick"))
+$button4 = ControlButton.new(SDL::Key::ESCAPE, Graphics.width - 66, Graphics.height - 66, 2, true)
+$button4.set_image_default(RPG::Cache.neko_control("default/UltimateDroidButton4.png"))
+$button4.set_image_pressed(RPG::Cache.neko_control("default/UltimateDroidButton4Pressed"))
+
+=begin
+$dpad = ControlDirection4.new(Graphics.width - 224, Graphics.height - 224, 0)
+$dpad.set_image_default(RPG::Cache.neko_control("default/dpad_none"))
+$dpad.set_image_down(RPG::Cache.neko_control("default/dpad_down"))
+$dpad.set_image_left(RPG::Cache.neko_control("default/dpad_left"))
+$dpad.set_image_right(RPG::Cache.neko_control("default/dpad_right"))
+$dpad.set_image_up(RPG::Cache.neko_control("default/dpad_up"))
+$dpad.set_image_stick(24, RPG::Cache.neko_control("default/dpad_stick"))
+#$dpad.sprite.x = $dpad.sprite_stick.x = Graphics.width - 224
+=end
+
+$dpad2 = ControlDirection4.new(Graphics.width - 224, Graphics.height - 224, 0)
+$dpad2.set_image_default(RPG::Cache.neko_control("old/dpad_none"))
+$dpad2.set_image_down(RPG::Cache.neko_control("old/dpad_down"))
+$dpad2.set_image_left(RPG::Cache.neko_control("old/dpad_left"))
+$dpad2.set_image_right(RPG::Cache.neko_control("old/dpad_right"))
+$dpad2.set_image_up(RPG::Cache.neko_control("old/dpad_up"))
+$dpad2.set_image_stick(12, RPG::Cache.neko_control("old/dpad_stick"))
+
+$dpad8 = ControlDirection8.new(0, Graphics.height - 224, 0)
+$dpad8.set_image_default(RPG::Cache.neko_control("default/dpad_none"))
+$dpad8.set_image_lower_left(RPG::Cache.neko_control("default/dpad_lower_left"))
+$dpad8.set_image_down(RPG::Cache.neko_control("default/dpad_down"))
+$dpad8.set_image_lower_right(RPG::Cache.neko_control("default/dpad_lower_right"))
+$dpad8.set_image_left(RPG::Cache.neko_control("default/dpad_left"))
+$dpad8.set_image_right(RPG::Cache.neko_control("default/dpad_right"))
+$dpad8.set_image_upper_left(RPG::Cache.neko_control("default/dpad_upper_left"))
+$dpad8.set_image_up(RPG::Cache.neko_control("default/dpad_up"))
+$dpad8.set_image_upper_right(RPG::Cache.neko_control("default/dpad_upper_right"))
+$dpad8.set_image_stick(20, RPG::Cache.neko_control("default/dpad_stick"))
