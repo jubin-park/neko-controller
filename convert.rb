6137811dbf8e11f37edc59b94fbd8a244f87cfec
Jubin Park <jubin-park@naver.com>
2020-03-22 00:50:33 +0900

    Revise convert.rb

--- convert.rb
+++ convert.rb
@@ -2,10 +2,11 @@
 $:.unshift << File.dirname(__FILE__)
 
 output = File.open("neko-controller.rb", "w+");
-list = File.read("src/main.rb")
-list.each_line do |line|
-	if line =~ /^require \"(.*)\"/
-		output.write File.read("src/#{$1}.rb") << "\n\n";
+list = File.readlines("rpgxp/Data/script/main.rb")
+list.each_index do |i|
+	if list[i] =~ /^require \"(.*)\"/
+		output.write File.read("rpgxp/Data/script/#{$1}.rb") << "\n"
+		output.write "\n" if i < list.size - 1
 	end
 end
 output.close
