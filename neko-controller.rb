0e80e669cba14678efca3632bddd7e358b61dfe3
Jubin Park <jubin-park@naver.com>
2020-03-22 00:53:35 +0900

    Release neko-controller.rb

--- neko-controller.rb
+++ neko-controller.rb
@@ -1,32 +1,6 @@
 #===============================================================================
 if !$NEKO_RUBY.nil?
 #-------------------------------------------------------------------------------
-module Kernel
-  def print(*args)
-    return if args.inspect.to_s =~ /(bgm_play|bgs_play|se_play|me_play) (.*)\\n/
-    msgbox(*args)
-  end
-      
-  def p(*args)
-    str = String.new
-    args.each{ |s| str << (s.inspect.to_s) + "\n" }
-    SDL.showAlert(str)
-  end
-end
-  
-undef msgbox
-def msgbox(*args)
-  str = String.new
-  args.each{ |s| str << (s.nil? ? "nil" : s.to_s) + "\n" }
-  SDL.showAlert(str)
-end
-#-------------------------------------------------------------------------------
-end
-#===============================================================================
-
-#===============================================================================
-if !$NEKO_RUBY.nil?
-#-------------------------------------------------------------------------------
 module SDL
   class << self
     alias_method(:handle_pad_touch, :handlePadTouch)
